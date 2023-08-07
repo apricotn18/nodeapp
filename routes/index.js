7715952948e19326bca8400327cceeec10cf035b
野崎 杏子 <nozaki_kyoko@waku-2.com>
2023-08-07 17:51:08 +0900

    change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var items = [
+    { "text": "1st Post." },
+    { "text": "2nd Post." }
+  ];
+  res.render('index', {
+    title: 'node app',
+    items
+  });
 });
 
 module.exports = router;
