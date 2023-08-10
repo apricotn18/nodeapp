1c2502ced9a7c864a1fad1015502c583d0563bf6
野崎 杏子 <nozaki_kyoko@waku-2.com>
2023-08-10 18:33:39 +0900

    postでページ遷移できるように

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,37 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 var { Configuration, OpenAIApi } = require("openai");
 
+// bodyParser
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// openai
 var configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 var openai = new OpenAIApi(configuration);
 
-router.get('/', async(req, res, next) => {
-  var completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{role: "user", content: "おすすめの朝ごはん"}],
-  });
+console.log('http://localhost:3000/');
+
+router.post('/', async(req, res, next) => {
+  // var completion = await openai.createChatCompletion({
+  //   model: "gpt-3.5-turbo",
+  //   messages: [{role: "user", content: "おすすめの朝ごはん"}],
+  // });
 
   res.render('index', {
-    title: completion.data.choices[0].message.content,
+    type: req.body.type || 'breakfast',
+    // title: completion.data.choices[0].message.content,
+  });
+});
+
+router.get('/', async(req, res, next) => {
+  res.render('index', {
+    type: 'breakfast',
+    // title: completion.data.choices[0].message.content,
   });
 });
 
